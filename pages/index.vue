3c23e2a6ecd6d7970cca5baf8ddef19c6be20176
gaozhao <xxx@yy.com>
2023-12-05 15:35:31 +0800

    xxx

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>{{greet}}</div>
+  <button @click="click">change</button>
+</template>
+
+<script lang="ts" setup>
+const greet=ref('hi')
+
+const click = () => {
+  greet.value='hello'
+}
+</script>
+
+<style lang="scss" scoped></style>
